9ab03d56062c5307f7c5ea9461a0234839f57f5c
524727@mail.muni.cz <524727@mail.muni.cz>
2024-04-29 18:22:44 +0200

    Adding DTOs for front-back communication

---
+++ TournamentManager.Backend/DTOs/PlayerDataDto.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace TournamentManager.Backend.DTOs
+{
+    public struct PlayerDataDto
+    {
+        public required string Name { get; set; }
+        public required string Age { get; set; }
+        public required string Height { get; set; }
+        public required string Weight { get; set; }
+        public required int Position { get; set; }
+    }
+}
---
+++ TournamentManager.Backend/DTOs/TeamDataDto.cs
@@ -0,0 +1,13 @@
+﻿using TournamentManager.Backend.Structures;
+
+namespace TournamentManager.Backend.DTOs
+{
+    public struct TeamDataDto
+    {
+        public required string Name { get; set; }
+        public required string City { get; set; }
+        public required Colors Colors { get; set; }
+        public required string Abbrevation { get; set; }
+        public required List<Player> Players { get; set; }
+    }
+}
