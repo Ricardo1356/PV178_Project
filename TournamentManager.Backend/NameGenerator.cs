f8a7b4357033c9ea45b690b9cc7d525aebbc4e89
524727@mail.muni.cz <524727@mail.muni.cz>
2024-04-29 11:23:49 +0200

    Create P name generator

---
+++ TournamentManager.Backend/NameGenerator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class NameGenerator
+{
+    private static Random random = new Random();
+    private static List<string> firstNames = new List<string>
+    {
+        "Alex", "Mike", "James", "John", "Robert", "Michael", "William", "David", "Richard", "Joseph",
+        "Charles", "Thomas", "Chris", "Daniel", "Matthew", "Anthony", "Mark", "Donald", "Steven", "Paul"
+    };
+
+    private static List<string> lastNames = new List<string>
+    {
+        "Smith", "Johnson", "Williams", "Jones", "Brown", "Davis", "Miller", "Wilson", "Moore", "Taylor",
+        "Anderson", "Thomas", "Jackson", "White", "Harris", "Martin", "Thompson", "Garcia", "Martinez", "Robinson",
+        "Clark", "Rodriguez", "Lewis", "Lee", "Walker", "Hall", "Allen", "Young", "Hernandez", "King",
+        "Wright", "Lopez", "Hill", "Scott", "Green", "Adams", "Baker", "Gonzalez", "Nelson", "Carter",
+        "Mitchell", "Perez", "Roberts", "Turner", "Phillips", "Campbell", "Parker", "Evans", "Edwards", "Collins",
+        "Stewart", "Sanchez", "Morris", "Rogers", "Reed", "Cook", "Morgan", "Bell", "Murphy", "Bailey",
+        "Rivera", "Cooper", "Richardson", "Cox", "Howard", "Ward", "Torres", "Peterson", "Gray", "Ramirez",
+        "James", "Watson", "Brooks", "Kelly", "Sanders", "Price", "Bennett", "Wood", "Barnes", "Ross",
+        "Henderson", "Coleman", "Jenkins", "Perry", "Powell", "Long", "Patterson", "Hughes", "Flores", "Washington",
+        "Butler", "Simmons", "Foster", "Gonzales", "Bryant", "Alexander", "Russell", "Griffin", "Diaz", "Hayes"
+    };
+
+    public static string GenerateName()
+    {
+        string firstName = firstNames[random.Next(firstNames.Count)];
+        string lastName = lastNames[random.Next(lastNames.Count)];
+        return $"{firstName} {lastName}";
+    }
+}
