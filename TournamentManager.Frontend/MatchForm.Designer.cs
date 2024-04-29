63cbed01dcbdb9b8cc333d8d51b3eca860ba1a16
524727@mail.muni.cz <524727@mail.muni.cz>
2024-04-29 22:06:50 +0200

    Working match results

--- TournamentManager.Frontend/MatchForm.Designer.cs
+++ TournamentManager.Frontend/MatchForm.Designer.cs
@@ -31,57 +31,223 @@
             Team1Label = new Label();
             label2 = new Label();
             Team2Label = new Label();
-            numericUpDown1 = new NumericUpDown();
-            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            Team1ScoreNumericUpDown1 = new NumericUpDown();
+            Team2ScoreNumericUpDown3 = new NumericUpDown();
+            Team2ScoreNumericUpDown1 = new NumericUpDown();
+            Team2ScoreNumericUpDown2 = new NumericUpDown();
+            Team1ScoreNumericUpDown2 = new NumericUpDown();
+            Team1ScoreNumericUpDown3 = new NumericUpDown();
+            label1 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            Team1ScoreLabel = new Label();
+            label7 = new Label();
+            Team2ScoreLabel = new Label();
+            EndMatchButton = new Button();
+            ((System.ComponentModel.ISupportInitialize)Team1ScoreNumericUpDown1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)Team2ScoreNumericUpDown3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)Team2ScoreNumericUpDown1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)Team2ScoreNumericUpDown2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)Team1ScoreNumericUpDown2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)Team1ScoreNumericUpDown3).BeginInit();
             SuspendLayout();
             // 
             // Team1Label
             // 
             Team1Label.AutoSize = true;
-            Team1Label.Location = new Point(77, 32);
+            Team1Label.Font = new Font("Segoe UI", 20F);
+            Team1Label.Location = new Point(199, 23);
             Team1Label.Name = "Team1Label";
-            Team1Label.Size = new Size(38, 15);
+            Team1Label.Size = new Size(90, 37);
             Team1Label.TabIndex = 0;
             Team1Label.Text = "label1";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Font = new Font("Segoe UI", 15F);
-            label2.Location = new Point(224, 21);
+            label2.Font = new Font("Segoe UI", 20F);
+            label2.Location = new Point(364, 23);
             label2.Name = "label2";
-            label2.Size = new Size(30, 28);
+            label2.Size = new Size(41, 37);
             label2.TabIndex = 1;
             label2.Text = "vs";
             // 
             // Team2Label
             // 
             Team2Label.AutoSize = true;
-            Team2Label.Location = new Point(372, 34);
+            Team2Label.Font = new Font("Segoe UI", 20F);
+            Team2Label.Location = new Point(399, 23);
             Team2Label.Name = "Team2Label";
-            Team2Label.Size = new Size(38, 15);
+            Team2Label.Size = new Size(90, 37);
             Team2Label.TabIndex = 2;
             Team2Label.Text = "label3";
             // 
-            // numericUpDown1
+            // Team1ScoreNumericUpDown1
             // 
-            numericUpDown1.Location = new Point(109, 187);
-            numericUpDown1.Name = "numericUpDown1";
-            numericUpDown1.Size = new Size(38, 23);
-            numericUpDown1.TabIndex = 3;
+            Team1ScoreNumericUpDown1.Font = new Font("Segoe UI", 15F);
+            Team1ScoreNumericUpDown1.Location = new Point(251, 106);
+            Team1ScoreNumericUpDown1.Name = "Team1ScoreNumericUpDown1";
+            Team1ScoreNumericUpDown1.Size = new Size(49, 34);
+            Team1ScoreNumericUpDown1.TabIndex = 3;
+            Team1ScoreNumericUpDown1.ValueChanged += Team1ScoreNumericUpDown_ValueChanged;
+            // 
+            // Team2ScoreNumericUpDown3
+            // 
+            Team2ScoreNumericUpDown3.Font = new Font("Segoe UI", 15F);
+            Team2ScoreNumericUpDown3.Location = new Point(459, 237);
+            Team2ScoreNumericUpDown3.Name = "Team2ScoreNumericUpDown3";
+            Team2ScoreNumericUpDown3.Size = new Size(49, 34);
+            Team2ScoreNumericUpDown3.TabIndex = 4;
+            Team2ScoreNumericUpDown3.ValueChanged += Team2ScoreNumericUpDown3_ValueChanged;
+            // 
+            // Team2ScoreNumericUpDown1
+            // 
+            Team2ScoreNumericUpDown1.Font = new Font("Segoe UI", 15F);
+            Team2ScoreNumericUpDown1.Location = new Point(459, 104);
+            Team2ScoreNumericUpDown1.Name = "Team2ScoreNumericUpDown1";
+            Team2ScoreNumericUpDown1.Size = new Size(49, 34);
+            Team2ScoreNumericUpDown1.TabIndex = 6;
+            Team2ScoreNumericUpDown1.ValueChanged += Team2ScoreNumericUpDown1_ValueChanged;
+            // 
+            // Team2ScoreNumericUpDown2
+            // 
+            Team2ScoreNumericUpDown2.Font = new Font("Segoe UI", 15F);
+            Team2ScoreNumericUpDown2.Location = new Point(459, 172);
+            Team2ScoreNumericUpDown2.Name = "Team2ScoreNumericUpDown2";
+            Team2ScoreNumericUpDown2.Size = new Size(49, 34);
+            Team2ScoreNumericUpDown2.TabIndex = 5;
+            Team2ScoreNumericUpDown2.ValueChanged += Team2ScoreNumericUpDown2_ValueChanged;
+            // 
+            // Team1ScoreNumericUpDown2
+            // 
+            Team1ScoreNumericUpDown2.Font = new Font("Segoe UI", 15F);
+            Team1ScoreNumericUpDown2.Location = new Point(251, 172);
+            Team1ScoreNumericUpDown2.Name = "Team1ScoreNumericUpDown2";
+            Team1ScoreNumericUpDown2.Size = new Size(49, 34);
+            Team1ScoreNumericUpDown2.TabIndex = 8;
+            Team1ScoreNumericUpDown2.ValueChanged += Team1ScoreNumericUpDown2_ValueChanged;
+            // 
+            // Team1ScoreNumericUpDown3
+            // 
+            Team1ScoreNumericUpDown3.Font = new Font("Segoe UI", 15F);
+            Team1ScoreNumericUpDown3.Location = new Point(251, 239);
+            Team1ScoreNumericUpDown3.Name = "Team1ScoreNumericUpDown3";
+            Team1ScoreNumericUpDown3.Size = new Size(49, 34);
+            Team1ScoreNumericUpDown3.TabIndex = 7;
+            Team1ScoreNumericUpDown3.ValueChanged += Team1ScoreNumericUpDown3_ValueChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 20F);
+            label1.Location = new Point(90, 101);
+            label1.Name = "label1";
+            label1.Size = new Size(155, 37);
+            label1.TabIndex = 9;
+            label1.Text = "First Period:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 20F);
+            label3.Location = new Point(58, 169);
+            label3.Name = "label3";
+            label3.Size = new Size(193, 37);
+            label3.TabIndex = 10;
+            label3.Text = "Second Period:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 20F);
+            label4.Location = new Point(78, 234);
+            label4.Name = "label4";
+            label4.Size = new Size(167, 37);
+            label4.TabIndex = 11;
+            label4.Text = "Third Period:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 25F);
+            label5.Location = new Point(79, 352);
+            label5.Name = "label5";
+            label5.Size = new Size(221, 46);
+            label5.TabIndex = 12;
+            label5.Text = "Match Result:";
+            // 
+            // Team1ScoreLabel
+            // 
+            Team1ScoreLabel.AutoSize = true;
+            Team1ScoreLabel.Font = new Font("Segoe UI", 25F);
+            Team1ScoreLabel.Location = new Point(344, 352);
+            Team1ScoreLabel.Name = "Team1ScoreLabel";
+            Team1ScoreLabel.Size = new Size(109, 46);
+            Team1ScoreLabel.TabIndex = 13;
+            Team1ScoreLabel.Text = "label6";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 25F);
+            label7.Location = new Point(378, 352);
+            label7.Name = "label7";
+            label7.Size = new Size(27, 46);
+            label7.TabIndex = 14;
+            label7.Text = ":";
+            // 
+            // Team2ScoreLabel
+            // 
+            Team2ScoreLabel.AutoSize = true;
+            Team2ScoreLabel.Font = new Font("Segoe UI", 25F);
+            Team2ScoreLabel.Location = new Point(399, 352);
+            Team2ScoreLabel.Name = "Team2ScoreLabel";
+            Team2ScoreLabel.Size = new Size(109, 46);
+            Team2ScoreLabel.TabIndex = 15;
+            Team2ScoreLabel.Text = "label8";
+            // 
+            // EndMatchButton
+            // 
+            EndMatchButton.Location = new Point(265, 413);
+            EndMatchButton.Name = "EndMatchButton";
+            EndMatchButton.Size = new Size(220, 44);
+            EndMatchButton.TabIndex = 16;
+            EndMatchButton.Text = "End Match";
+            EndMatchButton.UseVisualStyleBackColor = true;
+            EndMatchButton.Click += EndMatchButton_Click;
             // 
             // MatchForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(598, 469);
-            Controls.Add(numericUpDown1);
+            AutoSize = true;
+            ClientSize = new Size(616, 471);
+            Controls.Add(EndMatchButton);
+            Controls.Add(Team2ScoreLabel);
+            Controls.Add(label7);
+            Controls.Add(Team1ScoreLabel);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label1);
+            Controls.Add(Team1ScoreNumericUpDown2);
+            Controls.Add(Team1ScoreNumericUpDown3);
+            Controls.Add(Team2ScoreNumericUpDown1);
+            Controls.Add(Team2ScoreNumericUpDown2);
+            Controls.Add(Team2ScoreNumericUpDown3);
+            Controls.Add(Team1ScoreNumericUpDown1);
             Controls.Add(Team2Label);
             Controls.Add(label2);
             Controls.Add(Team1Label);
             Name = "MatchForm";
             Text = "MatchForm";
-            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)Team1ScoreNumericUpDown1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)Team2ScoreNumericUpDown3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)Team2ScoreNumericUpDown1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)Team2ScoreNumericUpDown2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)Team1ScoreNumericUpDown2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)Team1ScoreNumericUpDown3).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -91,6 +257,19 @@
         private Label Team1Label;
         private Label label2;
         private Label Team2Label;
-        private NumericUpDown numericUpDown1;
+        private NumericUpDown Team1ScoreNumericUpDown1;
+        private NumericUpDown Team2ScoreNumericUpDown3;
+        private NumericUpDown Team2ScoreNumericUpDown1;
+        private NumericUpDown Team2ScoreNumericUpDown2;
+        private NumericUpDown Team1ScoreNumericUpDown2;
+        private NumericUpDown Team1ScoreNumericUpDown3;
+        private Label label1;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label Team1ScoreLabel;
+        private Label label7;
+        private Label Team2ScoreLabel;
+        private Button EndMatchButton;
     }
 }
--- TournamentManager.Frontend/MatchForm.cs
+++ TournamentManager.Frontend/MatchForm.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,19 +16,86 @@ namespace TournamentManager.Frontend
     public partial class MatchForm : Form
     {
         private BackendMain Backend;
-        private Team Team1;
-        private Team Team2;
+        private Team _team1;
+        private Team _team2;
+        public decimal Team1Score = 0;
+        public decimal Team2Score = 0;
 
-        public Team Winner { get; set; } = new Team();
         public MatchForm(BackendMain backend, Team team1, Team team2)
         {
-            Backend = backend;
-            Team1 = team1;
-            Team2 = team2;
-            Winner = Team1;
             InitializeComponent();
+            Backend = backend;
+            _team1 = team1;
+            _team2 = team2;
+            Team1Label.Text = $"{team1.City} {team1.Name}";
+            Team2Label.Text = $"{team2.City} {team2.Name}";
+            Team1ScoreLabel.Text = 0.ToString();
+            Team2ScoreLabel.Text = 0.ToString();
+            PositionLabels();
         }
 
+        private void PositionLabels()
+        {
+            Team1Label.Left = label2.Left - Team1Label.Width - 10;
+            Team1Label.Top = label2.Top;
 
+            Team2Label.Left = label2.Right + 10;
+            Team2Label.Top = label2.Top;
+        }
+
+        private void ChangeScoreTeam1()
+        {
+            Team1Score = Team1ScoreNumericUpDown1.Value + Team1ScoreNumericUpDown2.Value + Team1ScoreNumericUpDown3.Value;
+            Team1ScoreLabel.Text = Team1Score.ToString();
+            Team1ScoreLabel.Left = label7.Left - Team1ScoreLabel.Width - 10;
+            Team1ScoreLabel.Top = label7.Top;
+        }
+        private void ChangeScoreTeam2()
+        {
+            Team2Score = Team2ScoreNumericUpDown1.Value + Team2ScoreNumericUpDown2.Value + Team2ScoreNumericUpDown3.Value;
+            Team2ScoreLabel.Text = Team2Score.ToString();
+            Team2ScoreLabel.Left = label7.Right + 10;
+            Team2ScoreLabel.Top = label7.Top;
+        }
+
+        private void Team1ScoreNumericUpDown_ValueChanged(object sender, EventArgs e)
+        {
+            ChangeScoreTeam1();
+        }
+
+        private void Team1ScoreNumericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            ChangeScoreTeam1();
+        }
+
+        private void Team1ScoreNumericUpDown3_ValueChanged(object sender, EventArgs e)
+        {
+            ChangeScoreTeam1();
+        }
+
+        private void Team2ScoreNumericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            ChangeScoreTeam2();
+        }
+
+        private void Team2ScoreNumericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            ChangeScoreTeam2();
+        }
+
+        private void Team2ScoreNumericUpDown3_ValueChanged(object sender, EventArgs e)
+        {
+            ChangeScoreTeam2();
+        }
+
+        private void EndMatchButton_Click(object sender, EventArgs e)
+        {
+            if (Team1Score == Team2Score)
+            {
+                MessageBox.Show("The match can't end in a draw.");
+                return;
+            }
+            this.Close();   
+        }
     }
 }
