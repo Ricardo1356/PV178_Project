37bf68afc107c00e9ecae216ee4d24ca3b27b3b6
524727@mail.muni.cz <524727@mail.muni.cz>
2024-06-07 14:26:42 +0200

    New DTOs for tournament saving

---
+++ TournamentManager.Backend/DTOs/DuelDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TournamentManager.Backend.DTOs
+{
+    public class DuelDto
+    {
+        public string? Team1 { get; set; }
+        public string? Team2 { get; set; }
+        public bool IsFinished { get; set; }
+        public string? Winner { get; set; }
+        public string? Loser { get; set; }
+        public int Team1Score { get; set; }
+        public int Team2Score { get; set; }
+    }
+}
---
+++ TournamentManager.Backend/DTOs/TournamentDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TournamentManager.Backend.DTOs
+{
+    public class TournamentDto
+    {
+        public List<string> TeamNames { get; set; }
+        public List<List<DuelDto>> Duels { get; set; }
+        public string Name { get; set; }
+        public bool IsFinished { get; set; }
+        public string Type { get; set; }
+    }
+}
